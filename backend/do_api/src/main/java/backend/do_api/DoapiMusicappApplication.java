5e9313942a7feb3cddd2c1117fc2c9ad5730c7a6
lee23 <71886857+schummLee@users.noreply.github.com>
2023-07-10 15:06:44 +0200

    update
    project date 07/14/23 to current

---
+++ backend/do_api/src/main/java/backend/do_api/DoapiMusicappApplication.java
@@ -0,0 +1,13 @@
+package backend.do_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoapiMusicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoapiMusicappApplication.class, args);
+	}
+
+}
---
+++ backend/do_api/src/test/java/backend/do_api/DoapiMusicappApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.do_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoapiMusicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
